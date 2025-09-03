8c81aa4e0c4ed5405eb49a4f939b18e686af7b42
cdorin-mcc <cdorin@mchenry.edu>
2025-09-02 19:58:40 -0500

    Py - Literal Values

---
+++ Literals.py
@@ -0,0 +1,22 @@
+
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 1500)
+
+# Integer
+y = -35
+print("Integer:", y)
+
+# Float
+z = 30.01
+print("Float:", z)
+
+# String
+name = "Chris"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
