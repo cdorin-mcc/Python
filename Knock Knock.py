584047956c67f421ad8b6f40d60364be78ab34cf
cdorin-mcc <cdorin@mchenry.edu>
2025-09-02 19:14:51 -0500

    Py Knock Knock Joke

---
+++ Knock Knock.py
@@ -0,0 +1,12 @@
+# Knock Knock Joke - CD
+print("Knock, Knock.");
+print("Who's there?");
+print("Lettuce.");
+print("Lettuce who?");
+print("Lettuce in, it's cold out here!");
+
+#Separator
+print("\n");
+
+#V2 Separated by "..." periods.
+print("Knock_Knock" , "Who's_there" , "Lettuce" , "Lettuce_who" , "Lettuce_in,_it's_cold_out_here!", sep="...");
