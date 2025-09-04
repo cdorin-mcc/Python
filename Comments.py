0831b6908698d8516494281bb164a2398f4166fd
cdorin-mcc <cdorin@mchenry.edu>
2025-09-04 17:52:16 -0500

    Comments Assignment

---
+++ Comments.py
@@ -0,0 +1,12 @@
+#Add a minimum of 4 comments to the Python program
+# to make it more understandable.
+
+tank_size = 15       #Listed in Gallons.
+# Define the total distance covered
+distance = 600       #Listed in Miles.
+# Calculate miles per gallon
+mpg = distance / tank_size
+# Print the result.  #mpg meaning miles per gallon.
+print("Miles per gallon:", mpg)
+
+#TODO: Add a conversion for km/per gallon or per liter.
