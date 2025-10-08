b47693ef70a944b1eed275aa0fe1518f376d87e8
cdorin-mcc <cdorin@mchenry.edu>
2025-10-07 22:07:29 -0500

    Square Numbers

--- PE1 4.7.py
+++ PE1 4.7.py
@@ -1,26 +1,12 @@
-#Create a script to track heart rate readings and calculate the average heart rate.
+# Simple Python program to calculate the square of a number
 
-#Defining time_slots as a list with times of day
-time_slots = ["Morning", "Midday", "Afternoon", "Evening"]
-
-#Defining heart_rates as an empty list to store heart rate readings
-heart_rates = []
-
-#Prompting the user to enter heart rate readings for each time slot
-for day in time_slots:
+def square_number():
     while True:
         try:
-            rate = int(input(f"Enter your heart rate for {day} (in bpm): "))
-            if rate <= 0:
-                print("Please enter a positive integer for heart rate.")
-            else:
-                heart_rates.append(rate)
-                break
+            number = input("Enter a number to square: ")
+            squared_number = int(number) ** 2
+            print(f"The square of {number} is {squared_number}.")
+            break
         except ValueError:
             print("Invalid input. Please enter a different numeric value.")
-
-#Calculating the average heart rate
-average_heart_rate = sum(heart_rates) / len(heart_rates)
-print(f"Your average heart rate for today is: {average_heart_rate:.2f} bpm")
-
-
+square_number()
